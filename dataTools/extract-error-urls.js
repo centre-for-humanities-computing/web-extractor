f93fe35508d34eff6359be788580feb6123c7e88
Midas Nouwens <au575125@d24550.eduroam.net.au.dk>
2020-01-28 12:45:21 +0100

    updated error urls extractor

--- dataTools/extract-error-urls.js
+++ dataTools/extract-error-urls.js
@@ -3,7 +3,6 @@ const readline = require('readline');
 const targetFile = 'errors.json';
 const newFileName = 'errorUrls.txt'
 
-let extractedUrls = []
 async function processLineByLine() {
   const fileStream = fs.createReadStream(targetFile);
 
@@ -11,8 +10,6 @@ async function processLineByLine() {
     input: fileStream,
     crlfDelay: Infinity
   });
-  // Note: we use the crlfDelay option to recognize all instances of CR LF
-  // ('\r\n') in input.txt as a single line break.
 
   for await (const line of rl) {
     let JSONline = JSON.parse(line);
